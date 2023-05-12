1ccb5976c0e93a877de8ab30ff0edfdc04b1c670
Kingsley B. Boafo <109430532+kayc0des@users.noreply.github.com>
2023-05-12 19:34:55 +0200

    js code added

--- LoginForm/scripts/scripts.js
+++ LoginForm/scripts/scripts.js
@@ -1,35 +1,31 @@
 /////////////////////////////////////////////////////
 /* Function to change DOM if user clicks on sign in */
-const signin = document.getElementById("signin");
+const signin = document.getElementById("existingUser");
 
 signin.addEventListener("click", function(event) {
   event.preventDefault();
   const header = document.getElementById("heading");
+  const formParent = document.getElementById("myForm");
+  const formChild1 = document.getElementById("signUpData");
+  const formChild2 = document.getElementById("confirmPassword");
+  const submitBtn = document.getElementById('submit');
+  const loginBtn = document.getElementById("login");
+  const signUp = document.getElementById("newUser");
+  const forgotPassword = document.getElementById("forgotPassword");
+
+
   header.innerHTML = "Sign in";
+  formParent.removeChild(formChild1); //Remove Last Name and First Name Fields
+  formParent.removeChild(formChild2); //Remove Confirm Password Field
+  submitBtn.style.display = 'none';
+  loginBtn.style.display = 'inline-block';
+  signin.style.display = "none";
+  signUp.style.display = 'inline-block';
+  forgotPassword.innerHTML = "Forgot Password?";
+  forgotPassword.setAttribute("href", "https://example.com");
 });
 
-////////////////////////////////////////////////////
-/* Form Validation 
-const submitBtn = document.getElementById("submit");
 
-submitBtn.addEventListener('click', function(event) {
-  event.preventDefault();
-  let fname = document.getElementById("fname").value;
-  let lname = document.getElementById("lname").value;
-  let email = document.getElementById("email").value;
-  let pwd = document.getElementById("pwd").value;
-  let pwd2 = document.getElementById("pwd2").value;
-  
-  if (fname == "" || lname == "" || email == "" || pwd == "" || pwd2 == "") {
-    alert("All fields are required");
-    return false;
-  }
-  if (pwd === pwd2) {
-    alert("Password do not match");
-    return false;
-  }
-  return true;
-}); */
 
 /////////////////////////////////////////////////////
 /* Try this  */
@@ -67,3 +63,26 @@ function validateForm() {
   }
   return true;
 }
+
+////////////////////////////////////////////////////
+/* Form Validation 
+const submitBtn = document.getElementById("submit");
+
+submitBtn.addEventListener('click', function(event) {
+  event.preventDefault();
+  let fname = document.getElementById("fname").value;
+  let lname = document.getElementById("lname").value;
+  let email = document.getElementById("email").value;
+  let pwd = document.getElementById("pwd").value;
+  let pwd2 = document.getElementById("pwd2").value;
+  
+  if (fname == "" || lname == "" || email == "" || pwd == "" || pwd2 == "") {
+    alert("All fields are required");
+    return false;
+  }
+  if (pwd === pwd2) {
+    alert("Password do not match");
+    return false;
+  }
+  return true;
+}); */
